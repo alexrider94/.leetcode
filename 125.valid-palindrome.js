9252ed9c9ce523d1f430f033cc751a6d37e41a0b
HYOJIN <38070150+alexrider94@users.noreply.github.com>
2021-03-19 18:33:16 +0900

    3
    3

--- 125.valid-palindrome.js
+++ 125.valid-palindrome.js
@@ -9,8 +9,18 @@
  * @param {string} s
  * @return {boolean}
  */
+
 var isPalindrome = function(s) {
-    
+    let d = s.toLowerCase().replace(/[^a-z0-9]/g,"");
+
+    let f = 0 , b = d.length-1;
+    for(let i = 0 ; i<d.length; ++i){
+        if(d[f] !== d[b]) return false;
+        f++;
+        b--;
+    }
+    return true;
 };
 // @lc code=end
-
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
+console.log(isPalindrome("ab_a"))
